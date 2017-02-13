362c9e39011d7c99d9c0cba86c71b9d60cbfe08e
Edgar Alarcon <Edgar Alarcon@192.168.1.101>
2017-02-13 10:52:04 -0500

    Inicializando primer repositorio

---
+++ src/aprendiendo/Aprendiendo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aprendiendo;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Edgar Alarcon
+ */
+public class Aprendiendo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.println ("Que hola mundo ni que verga ");
+  JOptionPane.showMessageDialog(null, "¡Hola mundo!");
+
+        // TODO code application logic here
+    }
+    
+}
